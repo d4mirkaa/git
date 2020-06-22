0c0563c91019c891c534a4719c8f8ab82b78c918
d4mirkaa <mavluytovdd@gmail.com>
2020-06-22 21:38:28 +0300

    Первый комит

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 //console.log(3)
 //console.log(2)
 
+function pow(x, n) {
+    var result = 1;
+
+    for (var i=0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+var x = promt('x?', '');
+var n =prompt('n?', '');
+
+if (n < 0) {
+    alert('hello')
+} else {
+    alert(pow(x, n));
+}
+
+alert('hello');
+[1,2,3].push()
+
+// однострочный коментарий
+
+/* многострочный 
+коментарий */
+
+// горячие клавиши ctrl+/
+
+alert(1);
+console.log(2);
+
 var myVar = 10;
 alert('agePerson:', agePerson);
 var camelCase =10;
